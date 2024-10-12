9a3922eb0cb4e6357c542475c28e0eb671e6710d
Darui99 <avangard4700@mail.ru>
2024-10-12 20:53:01 +0900

    Active shield if can be damaged

--- src/main/java/logic/Shield.java
+++ src/main/java/logic/Shield.java
@@ -1,5 +1,6 @@
 package logic;
 
+import model.MineTransport;
 import model.WorldInfo;
 import model.request.Request;
 
@@ -7,9 +8,18 @@ public class Shield {
     public static void setShields(WorldInfo info, Request req) {
         for (var we : req.transports) {
             var cur = info.transports.stream().filter(t -> t.id.equals(we.id)).findFirst().get();
-            if (cur.shieldCooldownMs < 100 && cur.shieldLeftMs < 1) {
+            if (cur.shieldCooldownMs < 100 && cur.shieldLeftMs < 1 && canBeDamaged(info, cur)) {
                 we.setActivateShield(true);
             }
         }
+    }
+
+    public static boolean canBeDamaged(WorldInfo info, MineTransport transport) {
+        for (var enemy : info.enemies) {
+            if (transport.distTo(enemy) <= info.attackRange + info.attackExplosionRadius) {
+                return true;
+            }
+        }
+        return false;
     }
 }
